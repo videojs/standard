a6b45ae9d980d842b7341a762320eb774c3e84e2
Pat O'Neill <pgoneill@gmail.com>
2016-07-27 16:17:50 -0400

    See ya, standard-engine

--- cli.js
+++ cli.js
@@ -1,4 +1,30 @@
 #!/usr/bin/env node
 
-var opts = require('./options.js');
-require('standard-engine').cli(opts);
+const findRoot = require('find-root');
+const path = require('path');
+const CLIEngine = require('eslint').CLIEngine;
+const cwd = process.cwd();
+
+var root;
+try {
+  root = findRoot(cwd);
+} catch (x) {}
+
+const pkg = root ? require(path.join(root, 'package.json')) : null;
+
+const cli = new CLIEngine({
+  cwd,
+  configFile: path.join(__dirname, 'eslintrc.json'),
+  ignorePattern: pkg && pkg.vjsstandard && pkg.vjsstandard.ignore || null
+});
+
+const files = process.argv.slice(2);
+
+if (!files.length) {
+  files.push('.');
+}
+
+const report = cli.executeOnFiles(files);
+const formatter = cli.getFormatter();
+
+console.log(formatter(report.results));
