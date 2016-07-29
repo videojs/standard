e3dfe4bfefcb7524921a880dd299f9f9ff6c8918
Pat O'Neill <pgoneill@gmail.com>
2016-07-29 11:49:19 -0400

    Add --format option, use commander
    This also adds the --version and --help options.

--- cli.js
+++ cli.js
@@ -1,30 +1,57 @@
 #!/usr/bin/env node
 
-const findRoot = require('find-root');
-const path = require('path');
+const commander = require('commander');
 const CLIEngine = require('eslint').CLIEngine;
-const cwd = process.cwd();
+const path = require('path');
+const os = require('os');
+const tsmlb = require('tsmlb');
+const ignores = require('./ignores');
+const pkg = require(path.join(__dirname, 'package.json'));
 
-var root;
-try {
-  root = findRoot(cwd);
-} catch (x) {}
+commander.
+  version(pkg.version).
+  option('--format', 'Format files where possible to comply with videojs-standard.').
+  arguments('[targets...]').
+  action(targets => {
+    commander.targets = targets;
+  }).
+  parse(process.argv);
 
-const pkg = root ? require(path.join(root, 'package.json')) : null;
-
-const cli = new CLIEngine({
-  cwd,
-  configFile: path.join(__dirname, 'eslintrc.json'),
-  ignorePattern: pkg && pkg.vjsstandard && pkg.vjsstandard.ignore || null
-});
-
-const files = process.argv.slice(2);
-
-if (!files.length) {
-  files.push('.');
+// If no targets were specified, default to this directory.
+if (!commander.targets) {
+  commander.targets = ['.'];
 }
 
-const report = cli.executeOnFiles(files);
-const formatter = cli.getFormatter();
+const cli = new CLIEngine({
+  cwd: process.cwd(),
+  configFile: path.join(__dirname, 'eslintrc.json'),
+  fix: Boolean(commander.format),
+  ignorePattern: ignores(process.cwd())
+});
 
-console.log(formatter(report.results));
+const report = cli.executeOnFiles(commander.targets);
+
+if (commander.format) {
+  CLIEngine.outputFixes(report);
+
+  const applied = report.results.
+    map(result => result.output ? result.filePath : '').
+    filter(Boolean);
+
+  if (applied.length) {
+    console.log(tsmlb`
+      Applied fixes to ${applied.length} files:
+        ${applied.join(os.EOL + '        ')}
+    `);
+  }
+
+  if (applied.length < report.results.length) {
+    console.log(tsmlb`
+      Could not apply fixes to ${applied.length ? report.results.length - applied.length : 'any'} files!
+    `);
+  }
+} else {
+  const formatter = cli.getFormatter();
+
+  console.log(formatter(report.results));
+}
