5dce8e36aeb1ded97d4dfe9ba5fcb34ed57e65ca
Brandon Casey <2381475+brandonocasey@users.noreply.github.com>
2020-09-30 14:17:31 -0400

    fix: always remove cache using root directory (#65)

--- remove-cache.js
+++ remove-cache.js
@@ -1,12 +1,14 @@
 /* eslint-disable no-console */
 const fs = require('fs');
 const path = require('path');
-
-const cache = path.join(process.cwd(), '.eslintcache');
+const findRoot = require('find-root');
 
 try {
-  fs.unlinkSync(cache);
-  console.log('removed .eslintcache');
+  const root = findRoot(process.env.INIT_CWD || process.cwd());
+  const cachePath = path.join(root, '.eslintcache');
+
+  fs.unlinkSync(cachePath);
+  console.log(`removed ${cachePath}`);
 } catch (e) {
   // file doesn't exist or we cannot delete.
 }
